b24861cf30990d54ab4a52b0076e55a3718030ad
Soran <antien.huang@gmail.com>
2024-09-19 23:32:12 +0100

    update navigation

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,9 +3,16 @@ import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
 
+export const pages = [
+  { name: "Experience", href: "/experience"},
+  { name: "Projects", href: "/projects" },
+  { name: "Contact", href: "/contact" },
+];
+
 export const Navigation: React.FC = () => {
 	const ref = useRef<HTMLElement>(null);
 	const [isIntersecting, setIntersecting] = useState(true);
+
 
 	useEffect(() => {
 		if (!ref.current) return;
@@ -27,20 +34,16 @@ export const Navigation: React.FC = () => {
 				}`}
 			>
 				<div className="container flex flex-row-reverse items-center justify-between p-6 mx-auto">
-					<div className="flex justify-between gap-8">
-						<Link
-							href="/projects"
-							className="duration-200 text-zinc-400 hover:text-zinc-100"
-						>
-							Projects
-						</Link>
-						<Link
-							href="/contact"
-							className="duration-200 text-zinc-400 hover:text-zinc-100"
-						>
-							Contact
-						</Link>
-					</div>
+          <div className="flex justify-between gap-8">
+            {pages.map((item) => (
+              <Link
+                href={item.href}
+                className="duration-200 text-zinc-400 hover:text-zinc-100"
+              >
+                {item.name}
+              </Link>
+            ))}
+          </div>
 
 					<Link
 						href="/"
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,28 +1,29 @@
 "use client";
-import { Github, Mail, Twitter } from "lucide-react";
+import { Github, Mail, Linkedin } from "lucide-react";
 import Link from "next/link";
 import { Navigation } from "../components/nav";
 import { Card } from "../components/card";
 
-const socials = [
+const links = [
 	{
-		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
-		label: "Twitter",
-		handle: "@chronark_",
+		icon: <Linkedin size={40} />,
+		href: "https://",
+		label: "LinkedIn",
+		handle: "An-Tien Soran Huang",
 	},
 	{
-		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		icon: <Mail size={40} />,
+		href: "mailto:antien.huang@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "antien.huang@gmail.com",
 	},
 	{
-		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		icon: <Github size={40} />,
+		href: "https://github.com/AnTienHuang",
 		label: "Github",
-		handle: "chronark",
+		handle: "AnTienHuang",
 	},
+
 ];
 
 export default function Example() {
@@ -31,10 +32,10 @@ export default function Example() {
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
 				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-3 lg:gap-16">
-					{socials.map((s) => (
+					{links.map((item) => (
 						<Card>
 							<Link
-								href={s.href}
+								href={item.href}
 								target="_blank"
 								className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16"
 							>
@@ -42,15 +43,15 @@ export default function Example() {
 									className="absolute w-px h-2/3 bg-gradient-to-b from-zinc-500 via-zinc-500/50 to-transparent"
 									aria-hidden="true"
 								/>
-								<span className="relative z-10 flex items-center justify-center w-12 h-12 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
-									{s.icon}
+								<span className="relative z-10 flex items-center justify-center w-20 h-20 text-sm duration-1000 border rounded-full text-zinc-200 group-hover:text-white group-hover:bg-zinc-900 border-zinc-500 bg-zinc-900 group-hover:border-zinc-200 drop-shadow-orange">
+									{item.icon}
 								</span>{" "}
 								<div className="z-10 flex flex-col items-center">
 									<span className="lg:text-xl font-medium duration-150 xl:text-3xl text-zinc-200 group-hover:text-white font-display">
-										{s.handle}
+										{item.handle}
 									</span>
 									<span className="mt-4 text-sm text-center duration-1000 text-zinc-400 group-hover:text-zinc-200">
-										{s.label}
+										{item.label}
 									</span>
 								</div>
 							</Link>
