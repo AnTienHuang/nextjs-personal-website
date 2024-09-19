d5fab7d6e6e48d8de5187ddce5ad5e45d71bd971
Soran <antien.huang@gmail.com>
2024-09-19 18:48:29 +0100

    add eslint

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Particles from "./components/particles";
 
 const navigation = [
+  { name: "Experience", href: "/experience"},
   { name: "Projects", href: "/projects" },
   { name: "Contact", href: "/contact" },
 ];
