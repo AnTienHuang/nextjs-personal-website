8ca596d4d79577debf736ca7772a443cbf76cf8d
Soran <antien.huang@gmail.com>
2024-10-14 16:29:58 +0100

    update roomradar content

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -6,25 +6,29 @@ const projects = [
     date: 'JUN 2024 ~ AUG 2024',
     order: 1,
     content: `
-    # Project 1
+RoomRadar.ai is an AI-powered hotel search application developed by [@Anchit](https://www.linkedin.com/in/anchitchandran/), [@Matt](https://www.linkedin.com/in/matt-peniket-6a051318a/) and [myself](https://www.linkedin.com/in/an-tien-huang/) as part of our MSc Computer Science project at University College London. Under the expert guidance of our supervisors, Dr. [@Yun_Fu](https://www.linkedin.com/in/dryunfu/) from UCL and Prof. [@Lee_Stott](https://linkedin.com/in/leestott/) from Microsoft, we created a cutting-edge solution that leverages Microsoft Azure infrastructure to deliver an exceptional user experience.
+Our application utilises Azure OpenAI and Azure AI Search to offer a multimodal interface, enabling users to search for hotels using text, voice, and images. Additionally, Azure Maps is also integrated to provide an interactive map for a more engaging and intuitive experience for our users.
+RoomRadar.ai showcases the potential of AI in enhancing travel planning, combining advanced technology with user-friendly design to revolutionise the way people find their ideal accommodations.
 
-This is the first project. It features:
+RoomRadar.ai was designed to enhance the hotel search user experience with features that traditional hotel search applications do not offer. The interactive map and image search are two of these features:
+ 
+- **Map View**
+  1. Display hotels on a map for an intuitive and visual way to explore search results
+  2. Include hotel cards in the side panel for a neat and organised display of hotel details.
+  3. Provide route planning information between the selected hotel and nearby underground stations, eliminating the need for users to check this separately.
+- **Image Search**
+  1. Allow users to use a base image to find hotels with similar visuals. This feature lets users express preferences that may be difficult to define using filters or text.
 
-- Amazing feature 1
-- Incredible feature 2
+![Map gif](/images/roomradar/map.gif)
+![Image Search gif](/images/roomradar/img_search.gif)
 
+The diagram below provides a high-level overview of RoomRadar’s architecture:
 ![Project 1 screenshot](/images/roomradar/aod.png)
 
-## Technical Details
+Details of the project can be found in my Microsoft [blog](https://techcommunity.microsoft.com/t5/educator-developer-blog/roomradar-ai-revolutionising-hotel-search-with-azure-maps-and/ba-p/4264443).
 
-This project was built using:
-
-1. React
-2. Next.js
-3. TypeScript
-
-Check out the [live demo](https://project1-demo.com)!
-    `
+I also wrote a tutorial for integrating Azure Maps with Next.js: [link](https://medium.com/@antien.huang/azure-maps-tutorial-with-nextjs-99f395f07a19?source=friends_link&sk=c3a20619f76e12fb7c55c76f97e2618f)
+`
   }
 ];
 
