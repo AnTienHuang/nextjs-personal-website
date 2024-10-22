97794c16ae57fc0997618ce6e168072814420032
Soran <antien.huang@gmail.com>
2024-10-22 17:59:12 +0100

    update project page header

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -48,7 +48,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 			className="relative isolate overflow-hidden bg-gradient-to-tl from-black via-zinc-900 to-black"
 		>
 			<div
-				className={`fixed inset-x-0 top-0 z-50 backdrop-blur lg:backdrop-blur-none duration-200 border-b lg:bg-transparent ${
+				className={`fixed inset-x-0 top-0 z-50 backdrop-blur duration-200 border-b lg:bg-transparent ${
 					isIntersecting
 						? "bg-zinc-900/0 border-transparent"
 						: "bg-white/10  border-zinc-200 lg:border-transparent"
@@ -56,7 +56,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 			>
 				<div className="container flex flex-row-reverse items-center justify-between p-10 mx-auto pt-15 max-w-6xl">
 					<div className="flex justify-between gap-8">
-						<span
+						{/* <span
 							title="View counter for this page"
 							className={`duration-200 hover:font-medium flex items-center gap-1 ${
 								isIntersecting
@@ -68,7 +68,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 							{Intl.NumberFormat("en-US", { notation: "compact" }).format(
 								views,
 							)}
-						</span>
+						</span> */}
 					</div>
 
 					<Link
